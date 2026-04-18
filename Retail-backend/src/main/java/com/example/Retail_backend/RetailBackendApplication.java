f60c98fe2fd60f2991c0669e2d5471aa517e4799
Shiv Awashthi <awashthishiv2002@gmail.com>
2026-04-18 11:38:25 +0530

    create intial folder

---
+++ Retail-backend/src/main/java/com/example/Retail_backend/RetailBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Retail_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailBackendApplication.class, args);
+	}
+
+}
