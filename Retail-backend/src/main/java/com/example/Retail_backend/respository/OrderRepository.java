a759c53e657c3a3906f57b557834e9ac47d616bc
Harshita Sinha <harshitassinha04@gmail.com>
2026-04-18 12:48:49 +0530

    Added repo method

--- Retail-backend/src/main/java/com/example/Retail_backend/respository/OrderRepository.java
+++ Retail-backend/src/main/java/com/example/Retail_backend/respository/OrderRepository.java
@@ -3,5 +3,8 @@ package com.example.Retail_backend.respository;
 import com.example.Retail_backend.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserUserId(Long userId);
 }
--- Retail-backend/src/main/java/com/example/Retail_backend/respository/ProductRepository.java
+++ Retail-backend/src/main/java/com/example/Retail_backend/respository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.example.Retail_backend.respository;
 import com.example.Retail_backend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryCategoryId(Long categoryId);
 }
